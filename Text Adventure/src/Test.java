b599ed17dae416739d5bafde91d250755b76abdb
Alex Lockhart <alex@dorway.net>
2014-02-12 16:57:59 -0400

    Test class added

---
+++ Text Adventure/src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * Test.java
+ * @author Stanford Lockhart
+ * B00646015
+ * Feb 12, 2014
+ * Dalhousie University
+ * Faculty of Computer Science
+ */
+public class Test {
+public static void main(String[] args) {
+	
+}
+}
