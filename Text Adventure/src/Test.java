f828649e852ff9c537620ce2bc0c5f1b90fafaeb
gcaven <geoffreycaven@gmail.com>
2014-02-12 17:20:01 -0400

    added message for Lockhart in test.java
    hi whats up

--- Text Adventure/src/Test.java
+++ Text Adventure/src/Test.java
@@ -4,7 +4,7 @@
 
 /**
  * Test.java
- * @author Stanford Lockhart
+ * @author Stanford Lockhart et. al
  * B00646015
  * Feb 12, 2014
  * Dalhousie University
@@ -15,5 +15,6 @@ public class Test {
 	public static void main(String[] args) {
 	
 		//Hello Matt
+		//I'm not Matt but I'm here anyway
 	}
 }
