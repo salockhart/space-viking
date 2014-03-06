854bcf9bb031974297d3ee65c05272b8a8cfa4e0
mattrask <mattrask34@gmail.com>
2014-03-06 00:40:01 -0400

    Added room class
    currently has a whole bunch of errors from accessing classes that have
    yet to exist. Will fix later

---
+++ Text Adventure/src/Room.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+public class Room {
+
+	/**
+	 * @author Matthew Trask B00639252
+	 */
+	
+	private boolean isTrap;
+	private boolean isLocked;
+	private ArrayList<Item> items;
+	private Person enemy;
+	private Player player;
+	private String description;
+	
+	
+	public Room(boolean isTrap, boolean isLocked, Item[] items, Person enemy,String description) {
+		this.isTrap = isTrap;
+		this.isLocked = isLocked;
+		this.items = items;
+		this.enemy = enemy;
+		this.description = description;
+	}
+	
+	public boolean isTrap() {
+		return isTrap;
+	}
+	
+	public boolean isLocked() {
+		return isLocked;
+	}
+	
+	public void unLock(boolean isLocked) {
+		this.isLocked = false;
+	}
+	
+	public Item[] getItems() {
+		return items;
+	}
+	
+	public void setItems(Item[] items) {
+		this.items = items;
+	}
+	
+	//this will likely not work as it's looking for item not item list I'll change once the item class is created
+	public void removeItems(String itemName) {
+		if(items.contains(itemName) {	
+			items.remove(itemName);
+		}
+	}
+	
+	public void addItems(Item adding) {
+		items.add(adding);
+	}
+	
+	public Person getEnemy() {
+		return enemy;
+	}
+	
+	public void setEnemy(Person enemy) {
+		this.enemy = enemy;
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+	
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
