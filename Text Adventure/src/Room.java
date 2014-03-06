384e422e8837acb897797b326197650765a633c4
Alex Lockhart <alex@dorway.net>
2014-03-06 17:59:33 -0400

    Added comments and toString, as well as a variable for tracking if the player has visited the room or not

--- Text Adventure/src/Room.java
+++ Text Adventure/src/Room.java
@@ -1,79 +1,164 @@
 import java.util.ArrayList;
+
+/**
+ * Room.java
+ * @author Matthew Trask
+ * B00639252
+ * Mar 5, 2014
+ * Dalhousie University
+ * Faculty of Computer Science
+ */
 public class Room {
 
-	/**
-	 * @author Matthew Trask B00639252
-	 */
-	
+	//Instance variables
 	private boolean isTrap;
 	private boolean isLocked;
 	private ArrayList<Item> items;
 	private Person enemy;
 	private Player player;
 	private String description;
+	private boolean hasVisited;
 	
-	
+	/**
+	 * Constructor to create the room 
+	 * @param isTrap
+	 * @param isLocked
+	 * @param items
+	 * @param enemy
+	 * @param description
+	 */
 	public Room(boolean isTrap, boolean isLocked, ArrayList<Item> items, Person enemy, String description) {
 		this.isTrap = isTrap;
 		this.isLocked = isLocked;
 		this.items = items;
 		this.enemy = enemy;
 		this.description = description;
+		this.hasVisited = false;
 	}
 	
+	/**
+	 * @return whether the room is trapped or not
+	 */
 	public boolean isTrap() {
 		return isTrap;
 	}
 	
+	/**
+	 * @return whether the room is locked or not
+	 */
 	public boolean isLocked() {
 		return isLocked;
 	}
 	
-	public void unLock(boolean isLocked) {
+	/**
+	 * @param isLocked
+	 * Unlocks the room
+	 */
+	public void unLock() {
 		this.isLocked = false;
 	}
 	
-	public Item[] getItems() {
+	/**
+	 * @return the array list of items in the room
+	 */
+	public ArrayList<Item> getItems() {
 		return items;
 	}
 	
+	/**
+	 * Set the items in the room to the given arraylist
+	 * @param items
+	 */
 	public void setItems(Item[] items) {
 		this.items = items;
 	}
 	
 	//this will likely not work as it's looking for item not item list I'll change once the item class is created
+	/**
+	 * Removes given Item from item list
+	 * @param itemName
+	 */
 	public void removeItems(String itemName) {
 		if(items.contains(itemName) {	
 			items.remove(itemName);
 		}
 	}
 	
+	/**
+	 * Adds a given Item to the items list
+	 * @param adding
+	 */
 	public void addItems(Item adding) {
 		items.add(adding);
 	}
 	
+	/**
+	 * @return the enemy object
+	 */
 	public Person getEnemy() {
 		return enemy;
 	}
 	
+	/**
+	 * Sets enemy in room to inputted Enemy
+	 * @param enemy
+	 */
 	public void setEnemy(Person enemy) {
 		this.enemy = enemy;
 	}
 	
+	/**
+	 * @return the Player object
+	 */
 	public Player getPlayer() {
 		return player;
 	}
 	
+	/**
+	 * Sets the player to given player object
+	 * @param player
+	 */
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
 	
+	/**
+	 * @return the room's String description
+	 */
 	public String getDescription() {
 		return description;
 	}
 	
+	/**
+	 * Sets the room's description to inputted String
+	 * @param description
+	 */
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	/**
+	 * @return whether player has visited the room
+	 */
+	public boolean hasVisited() {
+		return hasVisited;
+	}
+
+	/**
+	 * @param set the hasVisited variable to true
+	 */
+	public void playerVisits() {
+		this.hasVisited = true;
+	}
+
+	/**
+	 * Return the room as a string
+	 */
+	public String toString() {
+		if (hasVisited)
+			return "[*]";
+		else
+			return "[ ]";
 	}
 	
 }
