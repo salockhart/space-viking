dec38b6b00d20a15dbb15ea75078c366e796a044
Alex <alex@dorway.net>
2014-03-06 18:44:01 -0400

    Cleaned up the merge
    I suck so hard at github

--- Text Adventure/src/Room.java
+++ Text Adventure/src/Room.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.ArrayList;
 
 /**
@@ -70,7 +69,7 @@ public class Room {
 	 * Set the items in the room to the given arraylist
 	 * @param items
 	 */
-	public void setItems(Item[] items) {
+	public void setItems(ArrayList<Item> items) {
 		this.items = items;
 	}
 	
@@ -80,7 +79,7 @@ public class Room {
 	 * @param itemName
 	 */
 	public void removeItems(String itemName) {
-		if(items.contains(itemName) {	
+		if(items.contains(itemName)) {	
 			items.remove(itemName);
 		}
 	}
@@ -163,84 +162,3 @@ public class Room {
 	}
 	
 }
-=======
-import java.util.ArrayList;
-public class Room {
-
-	/**
-	 * @author Matthew Trask B00639252
-	 */
-	
-	private boolean isTrap;
-	private boolean isLocked;
-	private ArrayList<Item> items;
-	private Person enemy;
-	private Player player;
-	private String description;
-	
-	
-	public Room(boolean isTrap, boolean isLocked, Item[] items, Person enemy,String description) {
-		this.isTrap = isTrap;
-		this.isLocked = isLocked;
-		this.items = items;
-		this.enemy = enemy;
-		this.description = description;
-	}
-	
-	public boolean isTrap() {
-		return isTrap;
-	}
-	
-	public boolean isLocked() {
-		return isLocked;
-	}
-	
-	public void unLock(boolean isLocked) {
-		this.isLocked = false;
-	}
-	
-	public ArrayList<Item> getItems() {
-		return items;
-	}
-	
-	public void setItems(ArrayList<Item> items) {
-		this.items = items;
-	}
-	
-	//this will likely not work as it's looking for item not item list I'll change once the item class is created
-	public void removeItems(String itemName) {
-		if(items.contains(itemName)) {	
-			items.remove(itemName);
-		}
-	}
-	
-	public void addItems(Item adding) {
-		items.add(adding);
-	}
-	
-	public Person getEnemy() {
-		return enemy;
-	}
-	
-	public void setEnemy(Person enemy) {
-		this.enemy = enemy;
-	}
-	
-	public Player getPlayer() {
-		return player;
-	}
-	
-	public void setPlayer(Player player) {
-		this.player = player;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-}
->>>>>>> 601013ad2abf2431884301cc6829a4eb61a38a13
