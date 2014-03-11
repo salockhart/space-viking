c40506c04c3559515023ae622ee665a4450e80f1
mattrask <mattrask34@gmail.com>
2014-03-10 21:38:08 -0300

    Moved Colours and clear screen command into main method
    Bonus look at how colours are likely going to be used

--- Text Adventure/src/Game.java
+++ Text Adventure/src/Game.java
@@ -15,8 +15,8 @@ public class Game {
 		
 		Scanner keyboard = new Scanner(System.in);
 		
-		printMessage("Hello There Dunn");
-		
+		printMessage("The "+ YELLOW+ "mutated Viking "+ RESET+ "swung his blade hitting for "+ RED+ "65 "+ RESET+ "Damage \n");
+		printMessage("You have "+ GREEN+ " 105 "+ RESET + "health remaining\n");
 		
 		
 	}
@@ -31,4 +31,15 @@ public class Game {
 			}
 		}
 	}
+	
+	public static final String RESET = "\u001B[0m";
+	public static final String BLACK = "\u001B[30m";
+	public static final String RED = "\u001B[31m";
+	public static final String GREEN = "\u001B[32m";
+	public static final String YELLOW = "\u001B[33m";
+	public static final String BLUE = "\u001B[34m";
+	public static final String PURPLE = "\u001B[35m";
+	public static final String CYAN = "\u001B[36m";
+	public static final String WHITE = "\u001B[37m";
+	public static final String CLEAN = "\033[H\033[2J";
 }
