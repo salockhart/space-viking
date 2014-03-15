36f35c5808073d4f4288174f7b21e7a2d5c1b91d
DUNN <alexdunn_76@hotmail.com>
2014-03-15 16:01:12 -0300

    finally fixed all file errors on my branch across all files
    via copy and pasting code from master branch on the website

--- Text Adventure/src/Player.java
+++ Text Adventure/src/Player.java
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 import java.util.ArrayList;
 
 public class Player extends Person
 {
 	private ArrayList<Item>inventory;
 	private String saint;
-	
+
 	public Player(String name,String profession,int health,int strength,
 			int defense,Item weapon,ArrayList<Item> inventory,String saint)
 	{
@@ -18,76 +17,29 @@ public class Player extends Person
 	{
 		inventory.remove(index);
 	}
-	
+
 	public void pickupItem(Item item)
 	{
 		inventory.add(item);
 	}
-	
+
 	public ArrayList<Item> getInventory()
 	{
 		return inventory;
 	}
-	
+
 	public void printInventory()
 	{
 		System.out.println("\n\ninventory\n");
 	}
-	
+
 	public String getSaint()
 	{
 		return saint;
 	}
-	
+
 	public void setSaint(String saint)
 	{
 		this.saint=saint;
 	}
 }
-=======
-import java.util.ArrayList;
-
-public class Player extends Person
-{
-	private ArrayList<Item>inventory;
-	private String saint;
-	
-	public Player(String name,String profession,int health,int strength,
-			int defense,Item weapon,ArrayList<Item> inventory,String saint)
-	{
-		super(name,profession,health,strength,defense,weapon);
-		this.saint=saint;
-		inventory=new ArrayList<Item>();
-	}
-
-	public void dropItem(int index)
-	{
-		inventory.remove(index);
-	}
-	
-	public void pickupItem(Item item)
-	{
-		inventory.add(item);
-	}
-	
-	public ArrayList<Item> getInventory()
-	{
-		return inventory;
-	}
-	
-	public void printInventory()
-	{
-		System.out.println("\n\ninventory\n");
-	}
-	
-	public String getSaint()
-	{
-		return saint;
-	}
-	
-	public void setSaint(String saint)
-	{
-		this.saint=saint;
-	}
-}
->>>>>>> origin/lockhart
